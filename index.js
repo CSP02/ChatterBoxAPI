85029daa19d11cba4c38923ee96580bed0dc2428
csp <chandrapilla2002@gmail.com>
2026-04-15 19:18:19 +0530

    fixed  logger

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const mongoose = require("mongoose")
 const express = require("express")
 const router = require("./routes/routes")
 const cors = require("cors")
-const morgan = require('morgan');
-const logger = require('./config/logger');
+// const morgan = require('morgan');
+// const logger = require('./config/logger');
 
 const mongoPath = process.env.MONGOPATH
 
@@ -21,11 +21,11 @@ const database = mongoose.connection
  * ? Handling database events
  */
 database.on("error", error => {
-    logger.error(error);
+    console.log(error);
 })
 
 database.once("connected", () => {
-    logger.info("Database connected!");
+    console.log("Database connected!");
 })
 
 /**
@@ -44,12 +44,12 @@ const corsOptions = {
 apiServer.use(cors(corsOptions));
 
 apiServer.use("/api", router)
-apiServer.use(morgan('combined', {
-    stream: {
-        write: (message) => logger.info(message.trim())
-    }
-}));
+// apiServer.use(morgan('combined', {
+//     stream: {
+//         write: (message) => logger.info(message.trim())
+//     }
+// }));
 
 apiServer.listen("3001", () => {
-    logger.info("Listening to 3001")
+    console.log("Listening to 3001")
 })
